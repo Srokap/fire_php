e72e895cd4287676c7c91709686e2ea6d7a34008
Paweł Sroka <srokap@gmail.com>
2013-03-10 01:38:26 +0100

    Base commit with FirePHP version 0.3.2

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 fire_php
 ========
 
-Elgg plugin wrapping FirePHP library and integrating with Elgg core
+Elgg plugin wrapping FirePHP library and integrating with Elgg core. See also [FirePHP project page](http://www.firephp.org/)
+
+Useful links:
+* Firefox - http://www.getfirefox.com/
+* Firebug - http://www.getfirebug.com/
+* FirePHP - http://www.firephp.org/
+
---
+++ start.php
@@ -0,0 +1,33 @@
+<?php
+require_once(dirname(__FILE__) . '/vendors/FirePHPCore/FirePHP.class.php');
+require_once(dirname(__FILE__) . '/vendors/FirePHPCore/fb.php');
+
+function fire_php_error_handler($errno, $errmsg, $filename, $linenum, $vars) {
+	$fp = FirePHP::getInstance();
+	$fp->errorHandler($errno, $errmsg, $filename, $linenum, $vars);
+	return  _elgg_php_error_handler($errno, $errmsg, $filename, $linenum, $vars);
+}
+
+function fire_php_exception_handler($exception) {
+	$fp = FirePHP::getInstance();
+	$fp->exceptionHandler($exception);
+	return _elgg_php_exception_handler($exception);
+}
+
+//remove Elgg core handlers
+restore_error_handler();
+restore_exception_handler();
+
+//initialize FirePHP
+FirePHP::init();
+$fp = FirePHP::getInstance();
+$fp->registerErrorHandler();
+$fp->registerExceptionHandler();
+
+//remove FirePHP handlers
+restore_error_handler();
+restore_exception_handler();
+
+//register plugin final handlers
+set_error_handler('fire_php_error_handler');
+set_exception_handler('fire_php_exception_handler');
