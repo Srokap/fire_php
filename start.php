6cddacae7897aaa71078575a1e34ed9bc6e78e4c
Paweł Sroka <srokap@gmail.com>
2013-05-13 03:14:41 +0200

    Always allow manipulating settings by disabling FirePHP for these cases. Version bump.

--- start.php
+++ start.php
@@ -5,7 +5,18 @@ elgg_register_class('FB', dirname(__FILE__) . '/vendors/FirePHPCore/fb.php');
 function fire_php_boot() {
 	ElggFirePHP::init();
 	$fp = ElggFirePHP::getInstance();
-	$fp->registerHandlers();
+	
+	$handler = get_input('handler');
+	$page = get_input('page');
+	$action = get_input("action");
+
+	// always allow manipulating settings
+	$isSettingsPage = ($handler == 'admin') && (strpos($page, 'plugin_settings/fire_php') === 0);
+	$isSettingsAction = ($action == 'plugins/settings/save');
+	
+	if (!$isSettingsPage && !$isSettingsAction) {
+		$fp->registerHandlers();
+	}
 }
 
 fire_php_boot();
